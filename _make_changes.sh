05f4e8d9d7ccd88dc4f4a3b6aaac0ac7643be129
elangeloscuro1 <angeltapia07084759@gmail.com>
2019-01-16 01:46:53 -0800

    2019-01-16 01:46:53

--- _make_changes.sh
+++ _make_changes.sh
@@ -1,6 +1,6 @@
 git add .
-#read -p "Enter message: " userInput
-#[[ $userInput != '' ]] && git commit -m "$userInput" || git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
-git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
+read -p "Enter message: " userInput
+[[ $userInput != '' ]] && git commit -m "$userInput" || git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
+#git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
 git push
 read -p "Press [Enter] to continue..."
