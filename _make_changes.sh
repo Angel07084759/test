a04086a289f506acf546a6864e7ff1a838970b47
elangeloscuro1 <angeltapia07084759@gmail.com>
2019-01-16 01:56:41 -0800

    2019-01-16 01:56:41
    test: cannot open `test' (No such file or directory)

--- _make_changes.sh
+++ _make_changes.sh
@@ -1,6 +1,6 @@
 git add .
 read -p "Enter message: " userInput
-git commit -m "`date '+%Y-%m-%d %H:%M:%S'`" -m "$userInput"
+git commit -m "`date '+%Y-%m-%d %H:%M:%S'`" -m "`$userInput`"
 #git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
 git push
 read -p "Press [Enter] to continue..."
