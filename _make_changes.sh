2fcf4fe50afc55cf4ccf4f7a22b8ee9b182bab86
elangeloscuro1 <angeltapia07084759@gmail.com>
2019-01-16 02:13:22 -0800

    2019-01-16 02:13:22: This commit includes a date

--- _make_changes.sh
+++ _make_changes.sh
@@ -1,8 +1,6 @@
 git add .
-read -p "Enter message: " userInput
-#git commit -m "$userInput"
-git commit -m "`date '+%Y-%m-%d %H:%M:%S'`:$userInput"
-#git commit -m "`date '+%Y-%m-%d %H:%M:%S'`" -m "`userInput`"
 #git commit -m "`date '+%Y-%m-%d %H:%M:%S'`"
+read -p "Enter message: " userInput
+git commit -m "`date '+%Y-%m-%d %H:%M:%S'`: $userInput"
 git push
 read -p "Press [Enter] to continue..."
